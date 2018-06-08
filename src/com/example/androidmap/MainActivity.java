7d50f195a643fa40be88da9ef2eb3dfab65acf33
Miguel Kuckoski <miguelfreitas32@gmail.com>
2018-06-08 17:27:15 -0300

    Aplicado funcionalidades nos botões
    botões de localização atual e rota executada.

--- src/com/example/androidmap/MainActivity.java
+++ src/com/example/androidmap/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidmap;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -13,8 +14,10 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import android.app.Activity;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
 	private Location location;
 	private static GoogleMap map;
@@ -22,6 +25,9 @@ public class MainActivity extends Activity {
 	private static Marker marker;
 	private static List<LatLng> rotaExecutada;
 	private static LatLng currentPosition;
+	private Button rota;
+	private static Button atual;
+	private static View view;
 
 	@Override
 
@@ -30,73 +36,70 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment)).getMap();
+		markerOptions = new MarkerOptions();
+		rota = (Button) findViewById(R.id.btnRota);
+		atual = (Button) findViewById(R.id.btnLocalAtual);
+		rota.setOnClickListener(this);
+		atual.setOnClickListener(this);
 
 		location = LocationUtil.getLocation(this);
-		LatLng here = new LatLng(location.getLatitude(), location.getLongitude());
-
-		map.moveCamera(CameraUpdateFactory.newLatLngZoom(here, 15));
-		map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
-
-		markerOptions = new MarkerOptions();
-		markerOptions.position(here);
-
-		if (here.longitude != 0 || here.latitude != 0) {
-			marker = map.addMarker(
-					markerOptions.position(here).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_foot)));
-			marker.showInfoWindow();
-		}
-		
+		changeLocation(location);
 
 	}
 
 	public static void changeLocation(Location location) {
-		 map.clear();
+
 		if (location != null) {
 			LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
 			map.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+
+			if (MainActivity.currentPosition == null)
+				map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+
+			if (view == atual || view == null)
+				map.clear();
 
 			marker = map.addMarker(markerOptions.position(currentPosition)
 					.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_foot)));
 
 			marker.showInfoWindow();
 
-			// getRotaExecutada().add(currentPosition);
-			// MainActivity.currentPosition = currentPosition;
+			getRotaExecutada().add(currentPosition);
+			MainActivity.currentPosition = currentPosition;
 		}
-		
-		// TODO passar localização para os botões
 	}
 
-	// @Override
-	// public void onClick(View view) {
-	// map.clear();
-	// if (view == findViewById(R.id.btnLocalAtual)) {
-	// mostrarAtual();
-	// } else if (view == findViewById(R.id.btnRota)) {
-	// mostrarRota();
-	// }
-	// }
-	//
-	// private void mostrarAtual() {
-	// markerOptions.position(currentPosition);
-	// marker =
-	// map.addMarker(markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_foot)));
-	// }
-	//
-	// private void mostrarRota() {
-	// for (LatLng latLgn : getRotaExecutada()) {
-	// markerOptions = new MarkerOptions();
-	// markerOptions.position(latLgn);
-	// marker =
-	// map.addMarker(markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_foot)));
-	// }
-	//
-	// }
-	//
-	// public static List<LatLng> getRotaExecutada() {
-	// if (rotaExecutada == null)
-	// rotaExecutada = new ArrayList<LatLng>();
-	// return rotaExecutada;
-	// }
+	@Override
+	public void onClick(View view) {
+		MainActivity.view = view;
+		if (view == findViewById(R.id.btnLocalAtual)) {
+			mostrarAtual();
+		} else if (view == findViewById(R.id.btnRota)) {
+			mostrarRota();
+		}
+	}
+
+	private void mostrarAtual() {
+		map.clear();
+		markerOptions.position(currentPosition);
+		marker = map.addMarker(markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_foot)));
+		map.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+		map.animateCamera(CameraUpdateFactory.zoomTo(20), 2000, null);
+	}
+
+	private void mostrarRota() {
+		for (LatLng latLgn : getRotaExecutada()) {
+			markerOptions = new MarkerOptions();
+			markerOptions.position(latLgn);
+			marker = map.addMarker(markerOptions.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_foot)));
+		}
+		map.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
+	}
+
+	public static List<LatLng> getRotaExecutada() {
+		if (rotaExecutada == null)
+			rotaExecutada = new ArrayList<LatLng>();
+		return rotaExecutada;
+	}
 
 }
